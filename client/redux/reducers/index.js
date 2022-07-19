5df0cd156702f482cabc36a209b5cb43268118f8
mati-farias <mati.farias91@hotmail.com>
2022-07-19 12:28:15 -0300

    Redux settings

---
+++ client/redux/reducers/index.js
@@ -0,0 +1,21 @@
+// import actions types
+// import { GET_ALL_BOOKS } from '../actions/actionTypes'
+// initial states
+
+const InitialState = {
+	books: [],
+};
+
+const rootReducer = (state = InitialState, action) => {
+	switch (action.type) {
+		// case GET_ALL_BOOKS: {
+		//     return {
+		//         ...state,
+		//         books: action.payload
+		//     }
+		// }
+		default: {
+			state;
+		}
+	}
+};
