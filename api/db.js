4c9411c09057bc8f840416b6bb05dfd4af91cff6
Trinigv <99trini@gmail.com>
2022-07-18 22:59:18 -0300

    Adding Models to db.js

--- api/db.js
+++ api/db.js
@@ -39,7 +39,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 
 // En sequelize.models están todos los modelos importados como propiedades
 // Para relacionarlos hacemos un destructuring
-const { User } = sequelize.models;
+const { User, Books, Category, Comment, Favorite_List, Language, PurchaseOrder } = sequelize.models;
 
 // Aca vendrian las relaciones
 // Product.hasMany(Reviews);
