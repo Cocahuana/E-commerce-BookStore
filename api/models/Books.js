9bc4c09667f4426f35d6d2fe15c757b986b66669
mati-farias <mati.farias91@hotmail.com>
2022-07-19 00:11:04 -0300

    Installing sequelize and synchronizing all models at once in www file

--- api/models/Books.js
+++ api/models/Books.js
@@ -1,24 +1,29 @@
-const { DataTypes } = require('sequelize'); 
+const { DataTypes } = require('sequelize');
 
 module.exports = (sequelize) => {
- sequelize.define('Book', { //generates ID automatically    
-    title:{
-        type: DataTypes.STRING,
-        allowNull: false
-    }, 
-    author:{
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    price:{
-        type: DataTypes.FLOAT,
-        allowNull: false,
-    },
-    description:{
-        type: DataTyprs.TEXT,
-    },
-    rating:{
-        type: DataTypes.INTEGER, 
-    }
-}, {timestamps: false})
-}
+	sequelize.define(
+		'Book',
+		{
+			//generates ID automatically
+			title: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			author: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			price: {
+				type: DataTypes.FLOAT,
+				allowNull: false,
+			},
+			description: {
+				type: DataTypes.TEXT,
+			},
+			rating: {
+				type: DataTypes.INTEGER,
+			},
+		},
+		{ timestamps: false }
+	);
+};
