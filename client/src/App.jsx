8151debba55545be26f7030de87480003fb31218
Luciano Carducci <carduccilucianof@gmail.com>
2022-07-19 00:32:51 -0300

    rutas2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,32 @@
 
-import {BrowserRouter, Route, Switch} from "react-router-dom"
-import BookHolder from "./components/BookHolder/BookHolder"
-import './App.css'
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import books from "./components/books/books"
 import AboutUs from './components/AboutUs/AboutUs'
+import login from "./components/SignIN/SignIn"
+import nav from "./components/NavBar/NavBar"
+import footer from "./components/Footer/Footer"
+import register from "./components/SignUP/SignUp"
+import details from "./components/BookDetail/BookDetail"
+import landing from "./components/Landing/LandingPage"
+import './App.css'
+
 import { Route } from "react-router-dom";
 
 
 function App() {
-  return (  
-  <React.Fragment>
-    <switch>
-    <Route exact path="/" />
-    <Route exact path="/books" component={BookHolder}/>
-    <Route exact path="/:id"/>
-    <Route exact path="/register"/>
-    <Route exact path="/login"/>
-    <Route exact path="/us" component={AboutUs}/>
-    </switch>
-   </React.Fragment>
+  return (
+    <React.Fragment>
+      <switch>
+        <Route path="/" component={nav} />
+        <Route exact path="/" component={landing} />
+        <Route exact path="/books" component={books} />
+        <Route exact path="/:id" component={details} />
+        <Route exact path="/register" component={register} />
+        <Route exact path="/login" component={login} />
+        <Route exact path="/us" component={AboutUs} />
+        <Route path="/" component={footer} />
+      </switch>
+    </React.Fragment>
   )
 }
 
