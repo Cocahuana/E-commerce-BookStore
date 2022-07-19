159002b50627e568607bf0665fbd28bdd5a631a9
Ezequiel <ezequieldominguez11@gmail.com>
2022-07-19 13:26:37 -0300

    feat(api/bin/www) and (api/db.js) and (api/app.js): configuration for deploying the backend done

--- api/db.js
+++ api/db.js
@@ -2,15 +2,52 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-const sequelize = new Sequelize(
-	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/books`,
-	{
-		logging: false, // set to console.log to see the raw SQL queries
-		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-	}
-);
+/* Deploy backend start*/
+// Here we made the connection to the DB in Heroku
+// DB_NAME will be assigned by heroku randomly
+
+let sequelize =
+	process.env.NODE_ENV === 'production'
+		? new Sequelize({
+				database: DB_NAME,
+				dialect: 'postgres',
+				host: DB_HOST,
+				port: 5432,
+				username: DB_USER,
+				password: DB_PASSWORD,
+				pool: {
+					max: 3,
+					min: 1,
+					idle: 10000,
+				},
+				dialectOptions: {
+					ssl: {
+						require: true,
+						// Ref.: https://github.com/brianc/node-postgres/issues/2009
+						rejectUnauthorized: false,
+					},
+					keepAlive: true,
+				},
+				ssl: true,
+		  })
+		: new Sequelize(
+				`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/books`,
+				{ logging: false, native: false }
+		  );
+
+/* Deploy backend end*/
+
+//Commented code below is not longer needed
+
+// const sequelize = new Sequelize(
+// 	`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/books`,
+// 	{
+// 		logging: false, // set to console.log to see the raw SQL queries
+// 		native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+// 	}
+// );
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
