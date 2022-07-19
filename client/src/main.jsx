354fa01be0684d4ad6df1f6afee5309d990ded61
Luciano Carducci <carduccilucianof@gmail.com>
2022-07-19 13:49:34 -0300

    bugs_fixed

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from "axios"
+import store from "./../redux/store/index"
 import { BrowserRouter } from 'react-router-dom';
 import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
@@ -7,9 +9,9 @@ import './index.css';
 
 /* Deploy front Start */
 import dotenv from 'dotenv';
-dotenv.config();
+// dotenv.config();
 //We need to settle the port for back when we work with the actions. REACT_APP is mandatory, otherwise, it will not work properly
-axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
+// axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
 /* Deploy front End */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
