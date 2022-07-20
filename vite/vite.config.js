d3119a75061eedf27e0b993dbf543ad28c7c0c9b
Ezequiel <ezequieldominguez11@gmail.com>
2022-07-19 21:46:19 -0300

    Apa?

--- vite/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-// import vue from '@vitejs/plugin-vue';
-
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-});
