83dec08a3301a5c63ba087a6de0c7b3c2ce2079a
Trinigv <99trini@gmail.com>
2022-07-20 11:12:24 -0300

    FillDbCorrection

--- api/controllers/bookControllers.js
+++ api/controllers/bookControllers.js
@@ -6,31 +6,36 @@ const {Books} = require('../db')
 const getPopularBooks = async (req, res) => {
     var apiData = await axios.get('https://api.nytimes.com/svc/books/v3/lists/overview.json?published_date=2022-07-19&api-key=ibDJiDwWad3JmgHfVm1ZQo0AgS6hbCMJ');
     var wash = await apiData.data.results.lists[0].books?.map(b => {
-        return {
+        return{
             title: b.title,
             author: b.author,
-            description: b.description}
+            description: b.description,
+            rating: 5,
+            image: b.book_image }
         });
     res.send(wash)
 };
-    
 
 
 const addBooksTodb = async () => {
+    var count = await Books.count();
+    if(count === 0){
     const info = await axios.get('https://www.googleapis.com/books/v1/volumes?q=the&printType=books&filter=paid-ebooks&maxResults=40')
     const bookVolume = await info.data.items.map(b => {
-        const add = Books.create({
-            title: b.volumeInfo.title,
+        const add = Books.findOrCreate({
+            where:
+            {title: b.volumeInfo.title,
             authors: b.volumeInfo.authors?.join(','),
             description: b.volumeInfo.description,
-            rating: b.volumeInfo.averageRating,
-            image: b.volumeInfo.imageLinks.thumbnail,
-            preview: b.volumeInfo.previewLink    
-        })
-    })
-    return 
+        }   
+        });
+    });
+    }
+    return
 };
+
 const addBooksTodb2 = async () => {
+    const count2 = Books.count();
     const info = await axios.get('https://www.googleapis.com/books/v1/volumes?q=and&printType=books&filter=paid-ebooks&maxResults=40')
     const bookVolume = await info.data.items.map(b => {
         const add = Books.create({
@@ -40,8 +45,8 @@ const addBooksTodb2 = async () => {
             rating: b.volumeInfo.averageRating,
             image: b.volumeInfo.imageLinks.thumbnail,
             preview: b.volumeInfo.previewLink
-        })
-    })
+        });
+    });
     return 
 };
 
@@ -52,17 +57,6 @@ const addTotalBooks = async(req, res) => {
     res.send({total})
 }
 
-
-/*authros: b.authors.map(a => a),
-                description: b.description,
-                rating: b.averageRating*/
-
-/*return {
-            title: b.title,
-            author: b.authors.map(a => a),
-            description: b.description,
-            rating: b.averageRating } 
-*/
 
 
 module.exports = {getPopularBooks, addBooksTodb, addTotalBooks}
