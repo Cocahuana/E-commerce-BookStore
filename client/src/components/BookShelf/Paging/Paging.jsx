064bc08514d28576f86ca9582d6d0300668b86ec
Rodri Soriano <rosoria010398@gmail.com>
2022-07-21 23:52:32 -0300

    feat(paging) finished , pending active button on currentpage

--- client/src/components/BookShelf/Paging/Paging.jsx
+++ client/src/components/BookShelf/Paging/Paging.jsx
@@ -1,9 +1,65 @@
-import React from 'react'
+import React from 'react';
+import { PagButton } from './PagButton/PagButton';
+import { Flex, Icon } from '@chakra-ui/react';
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
-function Paging() {
-  return (
-    <div>Paging</div>
-  )
-}
-
-export default Paging
+export const Paging = ({
+	BooksPerPage,
+	TotalBooksLength,
+	setCurrentPage,
+	CurrentPage,
+}) => {
+	const pageNumbers = [];
+	for (let i = 1; i <= Math.ceil(TotalBooksLength / BooksPerPage); i++) {
+		pageNumbers.push(i);
+	}
+	return (
+		<Flex
+			bg='#edf3f8'
+			_dark={{
+				bg: '#3e3e3e',
+			}}
+			w='full'
+			alignItems='center'
+			justifyContent='center'>
+			<Flex>
+				<PagButton
+					onClick={() => {
+						if (CurrentPage != 1) setCurrentPage(CurrentPage - 1);
+					}}>
+					<Icon
+						as={IoIosArrowBack}
+						color='gray.700'
+						_dark={{
+							color: 'gray.200',
+						}}
+						boxSize={4}
+					/>
+				</PagButton>
+				{pageNumbers &&
+					pageNumbers.map((e) => (
+						<PagButton key={e} p onClick={() => setCurrentPage(e)}>
+							{e}
+						</PagButton>
+					))}
+				<PagButton
+					onClick={() => {
+						if (
+							CurrentPage !=
+							Math.ceil(TotalBooksLength / BooksPerPage)
+						)
+							setCurrentPage(CurrentPage + 1);
+					}}>
+					<Icon
+						as={IoIosArrowForward}
+						color='gray.700'
+						_dark={{
+							color: 'gray.200',
+						}}
+						boxSize={4}
+					/>
+				</PagButton>
+			</Flex>
+		</Flex>
+	);
+};
