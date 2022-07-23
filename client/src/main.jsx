9f340cba38f432e4fbbbcee006d4e5f5017417fe
Ezequiel <ezequieldominguez11@gmail.com>
2022-07-23 16:42:36 -0300

    Try to fix number 16. It will be this time???

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -14,7 +14,8 @@ import './components/theme/styleFont.css';
 // dotenv.config();
 //We need to settle the port for back when we work with the actions. REACT_APP is mandatory, otherwise, it will not work properly
 //In order to use Vite, we need to use import.meta.env instead of dotenv.config() and process.env
-axios.defaults.baseURL = import.meta.env.REACT_APP_API;
+axios.defaults.baseURL =
+	import.meta.env.VITE_APP_API || 'http://localhost:3001';
 /* Deploy front End */
 
 ReactDOM.createRoot(document.getElementById('root')).render(
