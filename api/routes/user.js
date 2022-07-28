54796d5231bbfc543ee937cecb49cccf57ef3279
GabrielDenis <83370739+GabrielDenis@users.noreply.github.com>
2022-07-28 20:22:45 -0300

    add, get and delete books to favorites in back

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const { Router } = require('express');
-const { 
-    registerUser,
-    userLogin,
-    searchUserByUsername,
+const {
+	registerUser,
+	userLogin,
+	searchUserByUsername,
+	addFavorite,
+	getFavorite,
+	deleteFavorite,
 } = require('../controllers/userControllers');
-
 
 const router = Router();
 
-router.post('/register', registerUser)
+router.post('/register', registerUser);
 
-router.post('/login', userLogin)
+router.post('/login', userLogin);
 
-router.get('/search/:username', searchUserByUsername)
+// router.get('/search/:username', searchUserByUsername);
 
+router.put('/favorites', addFavorite);
+
+router.get('/favorites/:idUser', getFavorite);
+
+router.delete('/favorites', deleteFavorite);
 
 module.exports = router;
