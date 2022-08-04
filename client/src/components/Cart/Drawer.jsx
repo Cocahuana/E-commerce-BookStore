a1516a9b7c116b7840032ab9a8422e22da477110
Ezequiel <ezequieldominguez11@gmail.com>
2022-08-03 22:57:37 -0300

    Onclick OnClose purchase now

--- client/src/components/Cart/Drawer.jsx
+++ client/src/components/Cart/Drawer.jsx
@@ -17,6 +17,7 @@ import { useDispatch } from 'react-redux';
 import { delAllCart } from '../../redux/actions/index';
 import { TiShoppingCart } from 'react-icons/ti';
 import SummaryPurchase from './SummaryPurchase';
+import { Link as BuenLink } from 'react-router-dom';
 
 const CartDrawer = () => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
@@ -47,6 +48,31 @@ const CartDrawer = () => {
 					<DrawerHeader></DrawerHeader>
 					<DrawerBody>
 						<Cart />
+						<Box p='10px' float='left'>
+							<BuenLink to='/purchase'>
+								<Button
+									onClick={onClose}
+									bg={useColorModeValue(
+										'blue.500',
+										'blue.200'
+									)}
+									color={useColorModeValue(
+										'white',
+										'gray.900'
+									)}
+									size='sm'
+									_hover={{
+										transform: 'translateY(2px)',
+										boxShadow: 'lg',
+										bg: useColorModeValue(
+											'gray.400',
+											'gray.600'
+										),
+									}}>
+									Purchase now!!
+								</Button>
+							</BuenLink>
+						</Box>
 						<Box p='10px' float='right'>
 							<Button
 								bg={useColorModeValue('blue.500', 'blue.200')}
