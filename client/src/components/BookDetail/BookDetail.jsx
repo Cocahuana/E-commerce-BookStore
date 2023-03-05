dddaa27fb802344664a886405315f97262f5913e
Cocahuana <ezequieldominguez11@gmail.com>
2023-03-05 17:37:58 -0300

    Delete: react import from BookDetail component

--- client/src/components/BookDetail/BookDetail.jsx
+++ client/src/components/BookDetail/BookDetail.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
 import {
 	addToCart,
 	getDetails,
@@ -9,8 +9,8 @@ import {
 	userAddFavorite,
 	userAddFavState,
 	userGetPurchases,
-} from '../../redux/actions';
-import { Link as BuenLink, useHistory } from 'react-router-dom';
+} from "../../redux/actions";
+import { Link as BuenLink, useHistory } from "react-router-dom";
 import {
 	Box,
 	Container,
@@ -30,14 +30,14 @@ import {
 	ModalCloseButton,
 	ModalBody,
 	ModalFooter,
-} from '@chakra-ui/react';
-import { AddIcon, EditIcon } from '@chakra-ui/icons';
-import { TiShoppingCart, TiHeartOutline } from 'react-icons/ti';
-import { Rating } from '../BookShelf/BookHolder/Book/Rating';
-import { PriceTag } from '../BookShelf/BookHolder/Book/PriceTag';
-import Swal from 'sweetalert2';
-import Reviews from './Reviews';
-import CommentPoster from './CommentPoster';
+} from "@chakra-ui/react";
+import { AddIcon, EditIcon } from "@chakra-ui/icons";
+import { TiShoppingCart, TiHeartOutline } from "react-icons/ti";
+import { Rating } from "../BookShelf/BookHolder/Book/Rating";
+import { PriceTag } from "../BookShelf/BookHolder/Book/PriceTag";
+import Swal from "sweetalert2";
+import Reviews from "./Reviews";
+import CommentPoster from "./CommentPoster";
 
 function BookDetail(props) {
 	const dispatch = useDispatch();
@@ -58,43 +58,43 @@ function BookDetail(props) {
 		});
 		if (flag) {
 			toast({
-				title: 'Added to the cart successfully',
-				status: 'success',
-				isClosable: 'true',
-				duration: '2000',
-				position: 'top',
+				title: "Added to the cart successfully",
+				status: "success",
+				isClosable: "true",
+				duration: "2000",
+				position: "top",
 			});
 		} else {
 			toast({
-				title: 'This book is already on the cart',
-				status: 'info',
-				isClosable: 'true',
-				duration: '2000',
-				position: 'top',
+				title: "This book is already on the cart",
+				status: "info",
+				isClosable: "true",
+				duration: "2000",
+				position: "top",
 			});
 		}
 	};
 
 	const handleOnFavourite = (id) => {
-		if (userId && userRole === 'User') {
+		if (userId && userRole === "User") {
 			dispatch(userAddFavorite(userId, id));
 			dispatch(userAddFavState(id));
-		} else if (userRole === 'Admin') {
+		} else if (userRole === "Admin") {
 			toast({
-				title: 'Admin can´t add favourites',
-				status: 'warning',
-				isClosable: 'true',
-				duration: '2000',
-				position: 'top',
+				title: "Admin can´t add favourites",
+				status: "warning",
+				isClosable: "true",
+				duration: "2000",
+				position: "top",
 			});
 		} else {
-			history.push('/login');
+			history.push("/login");
 			toast({
-				title: 'You need to be logged in to add to Favourites',
-				status: 'warning',
-				isClosable: 'true',
-				duration: '2000',
-				position: 'top',
+				title: "You need to be logged in to add to Favourites",
+				status: "warning",
+				isClosable: "true",
+				duration: "2000",
+				position: "top",
 			});
 		}
 	};
@@ -102,8 +102,8 @@ function BookDetail(props) {
 	useEffect(() => {
 		if (!allUsers.length) dispatch(getAllUsers());
 		dispatch(getDetails(id));
-		localStorage.setItem('cart', JSON.stringify(cart));
-		localStorage.setItem('summary', JSON.stringify(summary));
+		localStorage.setItem("cart", JSON.stringify(cart));
+		localStorage.setItem("summary", JSON.stringify(summary));
 		return () => {
 			dispatch(resetDetails());
 		};
@@ -117,11 +117,11 @@ function BookDetail(props) {
 
 	const handleOnClickRev = () => {
 		toast({
-			title: 'You need to be logged in to post comments',
-			status: 'warning',
-			isClosable: 'true',
-			duration: '2000',
-			position: 'top',
+			title: "You need to be logged in to post comments",
+			status: "warning",
+			isClosable: "true",
+			duration: "2000",
+			position: "top",
 		});
 	};
 
@@ -146,42 +146,45 @@ function BookDetail(props) {
 	if (details.stock > 0) {
 		return (
 			<Container
-				align={'center'}
-				bg={useColorModeValue('gray.200', 'gray.900')}
-				color={useColorModeValue('gray.700', 'whiteAlpha.600')}
-				minW={'100%'}
-				minH={'90vh'}
-				paddingTop={'10vh'}>
+				align={"center"}
+				bg={useColorModeValue("gray.200", "gray.900")}
+				color={useColorModeValue("gray.700", "whiteAlpha.600")}
+				minW={"100%"}
+				minH={"90vh"}
+				paddingTop={"10vh"}
+			>
 				<Box
-					maxW={'6xl'}
+					maxW={"6xl"}
 					textAlign='left'
 					border='1px'
-					borderColor={useColorModeValue('gray.200', 'gray.900')}>
+					borderColor={useColorModeValue("gray.200", "gray.900")}
+				>
 					<SimpleGrid
 						columns={{ base: 1, lg: 2 }}
 						rows={{ base: 3, lg: 3 }}
-						bg={useColorModeValue('whiteAlpha.600', 'gray.700')}
+						bg={useColorModeValue("whiteAlpha.600", "gray.700")}
 						//spacing={{ base: 8, md: 10 }}
 					>
 						<Flex>
 							<Flex
-								w={'100%'}
+								w={"100%"}
 								//Si le sacas maxHeight && los pixeles, la card se ajusta al dropdown del description
 								maxHeight={{
-									base: '500px',
-									sm: '400px',
-									lg: '700px',
+									base: "500px",
+									sm: "400px",
+									lg: "700px",
 								}}
-								align={'center'}
-								justify={'center'}>
+								align={"center"}
+								justify={"center"}
+							>
 								<Image
-									rounded={'md'}
-									alt={'book image'}
+									rounded={"md"}
+									alt={"book image"}
 									src={details?.image}
-									fit={'container'}
-									align={'center'}
-									w={{ base: '75%', sm: '75%', lg: '90%' }}
-									h={{ base: '90%', sm: '90%', lg: '90%' }}
+									fit={"container"}
+									align={"center"}
+									w={{ base: "75%", sm: "75%", lg: "90%" }}
+									h={{ base: "90%", sm: "90%", lg: "90%" }}
 								/>
 							</Flex>
 						</Flex>
@@ -189,19 +192,21 @@ function BookDetail(props) {
 							p={{ base: 10, sm: 10, md: 10, lg: 10 }}
 							border='1px'
 							borderColor={useColorModeValue(
-								'gray.200',
-								'gray.900'
+								"gray.200",
+								"gray.900"
 							)}
 							m='24px'
-							rounded='10px'>
-							<Box as={'header'}>
+							rounded='10px'
+						>
+							<Box as={"header"}>
 								<Text
-									fontWeight={'bold'}
+									fontWeight={"bold"}
 									fontSize={{
-										base: '1xl',
-										sm: '2xl',
-										lg: '3xl',
-									}}>
+										base: "1xl",
+										sm: "2xl",
+										lg: "3xl",
+									}}
+								>
 									{details?.title}
 								</Text>
 							</Box>
@@ -209,10 +214,11 @@ function BookDetail(props) {
 								<Text
 									w='100%'
 									fontSize={{
-										base: '24px',
-										sm: '24px',
-										lg: '36px',
-									}}>
+										base: "24px",
+										sm: "24px",
+										lg: "36px",
+									}}
+								>
 									<PriceTag
 										w='100%'
 										price={details.price}
@@ -223,12 +229,12 @@ function BookDetail(props) {
 							</Stack>
 
 							<Stack spacing={{ base: 4, sm: 6 }}>
-								<Text fontSize={'20px'}>
+								<Text fontSize={"20px"}>
 									{details?.authors}
 								</Text>
 
-								<Text fontSize={'20px'}>
-									<Text paddingTop={'9px'}>
+								<Text fontSize={"20px"}>
+									<Text paddingTop={"9px"}>
 										<Rating
 											size='20px'
 											defaultValue={details?.rating}
@@ -236,14 +242,15 @@ function BookDetail(props) {
 									</Text>
 								</Text>
 								<Flex
-									fontSize={{ base: '10px', lg: '20px' }}
+									fontSize={{ base: "10px", lg: "20px" }}
 									minH='100px'
-									align={'center'}>
+									align={"center"}
+								>
 									{details?.Genres?.map((e) => (
 										<Flex
 											bg={useColorModeValue(
-												'gray.200',
-												'gray.700'
+												"gray.200",
+												"gray.700"
 											)}
 											h='50px'
 											w='47%'
@@ -252,30 +259,32 @@ function BookDetail(props) {
 											justify='center'
 											border='1px'
 											borderColor={useColorModeValue(
-												'gray.200',
-												'gray.900'
+												"gray.200",
+												"gray.900"
 											)}
-											borderRadius='20px'>
+											borderRadius='20px'
+										>
 											{e.name}
 										</Flex>
 									))}
 								</Flex>
-								<Text fontSize={'20px'}>
+								<Text fontSize={"20px"}>
 									<Text>
-										{'Language: '}
+										{"Language: "}
 										{details?.Languages?.map(
 											(e) =>
 												e.name.charAt(0) +
 												e.name.slice(1).toLowerCase()
-										) + ''}
+										) + ""}
 									</Text>
 								</Text>
 							</Stack>
 							<Stack
-								py={'30px'}
-								justify={'center'}
-								align={'center'}>
-								<Text fontSize={'20px'}>
+								py={"30px"}
+								justify={"center"}
+								align={"center"}
+							>
+								<Text fontSize={"20px"}>
 									<Text>Stock Available:</Text>
 								</Text>
 								<Text>
@@ -288,29 +297,30 @@ function BookDetail(props) {
 										onClick={() =>
 											handleOnClick(details.id)
 										}
-										w={'100%'}
-										size={'lg'}
+										w={"100%"}
+										size={"lg"}
 										bg={useColorModeValue(
-											'blue.500',
-											'blue.200'
+											"blue.500",
+											"blue.200"
 										)}
 										color={useColorModeValue(
-											'white',
-											'gray.900'
+											"white",
+											"gray.900"
 										)}
 										leftIcon={<TiShoppingCart />}
-										textTransform={'uppercase'}
-										transition={'1s'}
+										textTransform={"uppercase"}
+										transition={"1s"}
 										_hover={{
 											bg: useColorModeValue(
-												'blue.200',
-												'blue.500'
+												"blue.200",
+												"blue.500"
 											),
 											color: useColorModeValue(
-												'gray.900',
-												'white'
+												"gray.900",
+												"white"
 											),
-										}}>
+										}}
+									>
 										Add to cart
 									</Button>
 								</Box>
@@ -319,62 +329,65 @@ function BookDetail(props) {
 										onClick={() =>
 											handleOnFavourite(details.id)
 										}
-										w={'100%'}
-										size={'lg'}
+										w={"100%"}
+										size={"lg"}
 										bg={useColorModeValue(
-											'blue.500',
-											'blue.200'
+											"blue.500",
+											"blue.200"
 										)}
 										color={useColorModeValue(
-											'white',
-											'gray.900'
+											"white",
+											"gray.900"
 										)}
 										leftIcon={<TiHeartOutline />}
-										textTransform={'uppercase'}
-										transition={'1s'}
+										textTransform={"uppercase"}
+										transition={"1s"}
 										_hover={{
 											bg: useColorModeValue(
-												'blue.200',
-												'blue.500'
+												"blue.200",
+												"blue.500"
 											),
 											color: useColorModeValue(
-												'gray.900',
-												'white'
+												"gray.900",
+												"white"
 											),
-										}}>
+										}}
+									>
 										Add to favourites
 									</Button>
 								</Box>
 								<Box
 									direction='row'
 									alignItems='center'
-									justifyContent={'center'}>
-									<BuenLink to={'/books'} w={'100%'}>
+									justifyContent={"center"}
+								>
+									<BuenLink to={"/books"} w={"100%"}>
 										<Button
-											align={'center'}
-											justify={'center'}
-											size={'lg'}
-											w={'100%'}
-											transition={'1s'}
+											align={"center"}
+											justify={"center"}
+											size={"lg"}
+											w={"100%"}
+											transition={"1s"}
 											color={useColorModeValue(
-												'#3483fa',
-												'gray.900'
+												"#3483fa",
+												"gray.900"
 											)}
 											bg={useColorModeValue(
-												'rgba(65, 137, 230, 0.35)',
-												'blue.200'
+												"rgba(65, 137, 230, 0.35)",
+												"blue.200"
 											)}
-											textTransform={'uppercase'}
+											textTransform={"uppercase"}
 											_hover={{
 												bg: useColorModeValue(
-													'rgba(65, 137, 230, 0.50)',
-													'rgba(65, 137, 230, 0.35)'
+													"rgba(65, 137, 230, 0.50)",
+													"rgba(65, 137, 230, 0.35)"
 												),
 												color: useColorModeValue(
-													'#3483fa',
-													'white'
+													"#3483fa",
+													"white"
 												),
-											}}>
+											}}
+										>
 											Home
 										</Button>
 									</BuenLink>
@@ -383,25 +396,27 @@ function BookDetail(props) {
 						</Stack>
 					</SimpleGrid>
 					<Stack
-						p={'20px'}
-						bg={useColorModeValue('whiteAlpha.600', 'gray.700')}
+						p={"20px"}
+						bg={useColorModeValue("whiteAlpha.600", "gray.700")}
 						direction='column'
 						alignItems='center'
-						justifyContent={'center'}>
+						justifyContent={"center"}
+					>
 						<Box
-							p={'5px'}
-							rounded={'5px'}
-							w={'100%'}
-							bg={useColorModeValue('blue.500', 'blue.200')}
-							color={useColorModeValue('white', 'gray.900')}
+							p={"5px"}
+							rounded={"5px"}
+							w={"100%"}
+							bg={useColorModeValue("blue.500", "blue.200")}
+							color={useColorModeValue("white", "gray.900")}
 							flex='1'
-							textAlign='center'>
+							textAlign='center'
+						>
 							Description
 						</Box>
 						<Text
 							textAlign='justify'
-							p={'10px'}
-							justifyContent={'center'}
+							p={"10px"}
+							justifyContent={"center"}
 							dangerouslySetInnerHTML={{
 								__html: details?.description,
 							}}
@@ -422,7 +437,7 @@ function BookDetail(props) {
 						<Text>We don´t have stock of this book</Text>
 					</ModalBody>
 					<ModalFooter>
-						<BuenLink to={'/books'}>
+						<BuenLink to={"/books"}>
 							<Button onClick={onClose}>Close</Button>
 						</BuenLink>
 					</ModalFooter>
